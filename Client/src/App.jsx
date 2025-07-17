ce1dd94f0b678fa6711ad07bec2f6bd7f307d996
vedant-35 <vedantbalpande023@gmail.com>
2025-07-17 19:05:19 +0530

    Deployment

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useSyncExternalStore } from "react";
 import io from "socket.io-client";
 import {Chat} from "./Chat";
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://chat-webapp-production-585a.up.railway.app/");
 
 const App = () => {
   const [username, setUsername] = useState("");
